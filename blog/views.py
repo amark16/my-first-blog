ff1821d211292683b3cc7e2a459a7290fba2d00c
Your Name <you@example.com>
2021-03-03 09:50:01 +0200

    added view and template for post detail page and CSS

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from .models import Post #We import Post model from models. Using dot before models uses models from current directory. 
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') #QuerySet
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
 # Create your views here.
